db78ccd8bee3201f8cbd37ba58b1e6c328426d55
Rafic <rjouejati@gmail.com>
2017-07-12 13:53:02 -0400

    Add files via upload
    Updated Main component

---
+++ app/components/Main.js
@@ -0,0 +1,21 @@
+var React = require("react");
+var Home = require("./Home");
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {
+      
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="container">
+        <div className="row">
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
