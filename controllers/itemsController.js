aca6c21bab2e188fdf9665970c848e0c47f17986
Aaron Bush <aaronbush100@gmail.com>
2017-07-15 13:35:28 -0400

    Update itemsController.js
    initial commit

---
+++ controllers/itemsController.js
@@ -0,0 +1,40 @@
+var Item = require("../models/item");
+
+module.exports = {
+  // This method handles retrieving items from the db
+  index: function(req, res) {
+    var query;
+    if (req.query) {
+      query = req.query;
+    }
+    else {
+      query = req.params.id ? { _id: req.params.id } : {};
+    }
+    Item.find(query)
+      .then(function(doc) {
+        res.json(doc);
+      }).catch(function(err) {
+        res.json(err);
+      });
+  },
+  // This method handles creating new items
+  create: function(req, res) {
+    Item.create(req.body).then(function(doc) {
+      res.json(doc);
+    }).catch(function(err) {
+      res.json(err);
+    });
+  },
+  // This method handles updating items
+  update: function(req, res) {
+    Item.update({
+      _id: req.params.id
+    },
+      req.body
+    ).then(function(doc) {
+      res.json(doc);
+    }).catch(function(err) {
+      res.json(err);
+    });
+  },
+};
