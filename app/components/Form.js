dc967b7ed72455a1163f95460fa1c05638499a62
Rafic <rjouejati@gmail.com>
2017-07-17 15:36:36 -0400

    Form.js

---
+++ app/components/Form.js
@@ -0,0 +1,51 @@
+var NameField = React.createClass({
+  getInitialState: function() {
+    return {
+      value: ''
+    };
+  },
+
+  onChange: function() {
+    this.setState({
+      value: e.target.value
+    })
+  },
+
+  clear: function() {
+    this.setState({
+      value: ''
+    })
+  },
+
+  render: function() {
+    return (
+      <input className='form-control' 
+        placeholder={this.props.type + ' Name'} 
+        onChange={this.onChange} value={this.state.value}/>
+    );
+  }
+});
+
+var EmailField = React.createClass({
+  getInitialState: function() {
+    return {
+      valid: true,
+      value: ''
+    } 
+  },
+
+  clear: function() {
+    this.setState({
+      valid: true,
+      value: ''
+    })
+  },
+
+  render: function() {
+    return (
+      <div className={formClass}>
+        <input className='form-control' onChange={this.onChange} placeholder='Email' value={this.state.value} />
+      </div>
+    )
+  }
+});
